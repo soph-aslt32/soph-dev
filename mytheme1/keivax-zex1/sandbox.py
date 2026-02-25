b67d1406e7b498520630c8abbc4d257a72a38cfd
soph-aslt32 <aslt3turnersoph@gmail.com>
2026-02-25 12:05:14 +0900

    [update]WIP

---
+++ mytheme1/keivax-zex1/sandbox.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+HOGE_GLOBAL = 1
+
+class Hoge:
+    def __init__(self, name: Any):
+        self.name = name
+
+    def greet(self):
+        return f"Hello, {self.name}!"
+
+def test_hoge():
+    hoge = Hoge("Test")
+    assert hoge.greet() == "Hello, Test!", "Hoge.greet() should return the correct greeting message."
+
+if __name__ == "__main__":
+    hoge  = Hoge("World")
+    print(hoge.greet())
+    print("This is a sandbox file for testing code snippets.")
+
+    bracket = ((((((((2))))))))
